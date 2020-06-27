3d9151346dac5e9c2b2db01bbc6d172ed98c0baa
Ghasel <65978885+Ghasel@users.noreply.github.com>
2020-06-27 11:46:23 +0200

    Add example code

---
+++ examples/streamlit_ace_demo.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from streamlit_ace import st_ace
+
+
+LANGUAGES = [
+    "abap", "abc", "actionscript", "ada", "alda", "apache_conf", "apex", "applescript", "aql", 
+    "asciidoc", "asl", "assembly_x86", "autohotkey", "batchfile", "c9search", "c_cpp", "cirru", 
+    "clojure", "cobol", "coffee", "coldfusion", "crystal", "csharp", "csound_document", "csound_orchestra", 
+    "csound_score", "csp", "css", "curly", "d", "dart", "diff", "django", "dockerfile", "dot", "drools", 
+    "edifact", "eiffel", "ejs", "elixir", "elm", "erlang", "forth", "fortran", "fsharp", "fsl", "ftl", 
+    "gcode", "gherkin", "gitignore", "glsl", "gobstones", "golang", "graphqlschema", "groovy", "haml", 
+    "handlebars", "haskell", "haskell_cabal", "haxe", "hjson", "html", "html_elixir", "html_ruby", "ini", 
+    "io", "jack", "jade", "java", "javascript", "json", "json5", "jsoniq", "jsp", "jssm", "jsx", "julia", 
+    "kotlin", "latex", "less", "liquid", "lisp", "livescript", "logiql", "logtalk", "lsl", "lua", "luapage", 
+    "lucene", "makefile", "markdown", "mask", "matlab", "maze", "mediawiki", "mel", "mixal", "mushcode", 
+    "mysql", "nginx", "nim", "nix", "nsis", "nunjucks", "objectivec", "ocaml", "pascal", "perl", "perl6", 
+    "pgsql", "php", "php_laravel_blade", "pig", "plain_text", "powershell", "praat", "prisma", "prolog", 
+    "properties", "protobuf", "puppet", "python", "qml", "r", "razor", "rdoc", "red", "redshift", "rhtml", 
+    "rst", "ruby", "rust", "sass", "scad", "scala", "scheme", "scss", "sh", "sjs", "slim", "smarty", 
+    "snippets", "soy_template", "space", "sparql", "sql", "sqlserver", "stylus", "svg", "swift", "tcl", 
+    "terraform", "tex", "text", "textile", "toml", "tsx", "turtle", "twig", "typescript", "vala", "vbscript", 
+    "velocity", "verilog", "vhdl", "visualforce", "wollok", "xml", "xquery", "yaml"
+]
+
+THEMES = [
+    "ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt", "crimson_editor", "dawn",
+    "dracula", "dreamweaver", "eclipse", "github", "gob", "gruvbox", "idle_fingers", "iplastic",
+    "katzenmilch", "kr_theme", "kuroir", "merbivore", "merbivore_soft", "mono_industrial", "monokai",
+    "nord_dark", "pastel_on_dark", "solarized_dark", "solarized_light", "sqlserver", "terminal",
+    "textmate", "tomorrow", "tomorrow_night", "tomorrow_night_blue", "tomorrow_night_bright",
+    "tomorrow_night_eighties", "twilight", "vibrant_ink", "xcode"
+]
+
+KEYBINDINGS = [
+    "emacs", "sublime", "vim", "vscode"
+]
+
+
+def main():
+    st.sidebar.title(":memo: Ace editor")
+
+    content = st_ace(
+        placeholder=st.sidebar.text_input("Editor placeholder.", value="Some placeholder."),
+        language=st.sidebar.selectbox("Language mode.", options=LANGUAGES),
+        theme=st.sidebar.selectbox("Theme.", options=THEMES),
+        keybinding=st.sidebar.selectbox("Keybinding mode.", options=KEYBINDINGS),
+        font_size=st.sidebar.slider("Font size.", 5, 24, 12),
+        tab_size=st.sidebar.slider("Tab size.", 1, 8, 4),
+        show_gutter=st.sidebar.checkbox("Show gutter.", value=True),
+        show_print_margin=st.sidebar.checkbox("Show print margin.", value=True),
+        wrap=st.sidebar.checkbox("Wrap enabled.", value=False),
+        readonly=st.sidebar.checkbox("Read-only.", value=False, key="ace-editor"),
+    )
+
+    st.write(content)
+
+
+if __name__ == "__main__":
+    main()
